13cddd3a0571ea39aad12f27386fcab5dcdc8b62
Kelly Santos <kellycristinasntos@gmail.com>
2024-02-04 17:21:09 -0300

    CSS aplicado

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import { useState } from 'react'
 import './App.css'
 
 import Input from './components/Input/Input'
+import Hi from './assets/bear-say-hi.png'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [email, setEmail] = useState('')
-
   return (
     <>
+    <div className='login-container'>
+
+   
      <h1>Bearbook</h1>
-     <Input tipo="text" placeholder="email@jmail.com" />
+     <img src={Hi} className='bear-img'/>
+     <div className='input-container'>
+     <Input tipo="email" placeholder="email@jmail.com" />
      <Input tipo="password" placeholder="******" />
      <Input tipo="button" placeholder="******" dado="Login"/>
+     </div>
+     </div>
     </>
   )
 }
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,7 +1,19 @@
 export default function Input({dado, tipo, placeholder}){
+ function bearHide(e){
+    let imgBear = document.querySelector('.bear-img')
+    if(e.target.type == 'password'){
+       imgBear.setAttribute('src', '/src/assets/bear-hide-password.png')
+    } else{
+        imgBear.setAttribute('src', '/src/assets/bear-say-hi.png')
+    }
+ }
+
+ function bearSee(){
+
+ }
     return(
         <>
-        <input type={tipo} value={dado} placeholder={placeholder} />
+        <input type={tipo} value={dado} placeholder={placeholder} onFocus={bearHide} onBlur={bearSee}/>
         </>
     )
 }

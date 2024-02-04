c165d83a3aff428b84af865b36d140b5c4fcbce9
Kelly Santos <kellycristinasntos@gmail.com>
2024-02-04 17:40:04 -0300

    footer adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-import Input from './components/Input/Input'
-import Hi from './assets/bear-say-hi.png'
+import Input from "./components/Input/Input";
+import Hi from "./assets/bear-say-hi.png";
 
 function App() {
   return (
     <>
-    <div className='login-container'>
-
-   
-     <h1>Bearbook</h1>
-     <img src={Hi} className='bear-img'/>
-     <div className='input-container'>
-     <Input tipo="email" placeholder="email@jmail.com" />
-     <Input tipo="password" placeholder="******" />
-     <Input tipo="button" placeholder="******" dado="Login"/>
-     </div>
-     </div>
+      <div className="login-container">
+        <h1>Bearbook</h1>
+        <img src={Hi} className="bear-img" />
+        <div className="input-container">
+          <Input tipo="email" placeholder="email@jmail.com" />
+          <Input tipo="password" placeholder="******" />
+          <Input tipo="button" placeholder="******" dado="Login" />
+          <footer>
+            <small>  © kellycdev</small>
+          </footer>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
